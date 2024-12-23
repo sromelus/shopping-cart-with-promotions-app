5fa265292bcf9769a1482e27161ab71c9d73d682
Shardly Romelus <rshardlypicard89@gmail.com>
2024-12-22 22:43:18 -0500

    added current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  helper_method :current_user, :current_user_cart
+
+  def current_user
+    @current_user ||= User.first
+  end
+
+  def current_user_cart
+    @current_user_cart ||= current_user.cart
+  end
 end
